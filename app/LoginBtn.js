4cf118e84662bd94ffc265c272c20a35a773e840
vividnow <belucent0@gmail.com>
2023-05-09 12:12:44 +0900

    추가, 로그아웃 기능

--- app/LoginBtn.js
+++ app/LoginBtn.js
@@ -1,10 +1,10 @@
 'use client'
 
-import { signIn, signOut } from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 
 export default function LoginBtn(){
     return (
-    <button onClick={()=> { signIn() }}>로그인</button>
-    
+        <button onClick={()=> { signIn() }}>로그인</button>
     )
 }
+
