abe4bd21741f493460963ed20535fd8b463014f6
vividnow <belucent0@gmail.com>
2023-06-01 02:59:27 +0900

    수정, 방명록 폼 간격 조정

--- app/guestbook/WriteForm.js
+++ app/guestbook/WriteForm.js
@@ -4,7 +4,7 @@ export default async function WriteForm({ userName}) {
     <>
       <form action="/api/guestbook/new" method="POST">
           <div className="flex mb-3">
-              <span className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{userName} </span>
+              <span className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-1.5 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">{userName} </span>
 
               <div className="relative w-full">
                   <input type="text" name="content" className="block p-3 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="100자 방명록" maxLength={100} minLength={2} required/>
