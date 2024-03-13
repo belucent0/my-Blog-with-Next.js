0b87fb5b8e678c2ed9da1ffe7b87f65c774b43ff
belucent0 <belucent0@gmail.com>
2024-03-13 17:14:51 +0900

    [config] eslint-config-next 추가, 이에 따른 설정 및 img->Image 태그 변경으로 이미지 최적화

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-    NEXT_PUBLIC_GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID
-  },
+    env: {
+        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+        NEXT_PUBLIC_GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID,
+    },
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'blog.kakaocdn.net'
-      },
-      {
-        protocol: 'https',
-        hostname: 's3.amazonaws.com',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "blog.kakaocdn.net",
+            },
+            {
+                protocol: "https",
+                hostname: "s3.amazonaws.com",
+            },
+            { protocol: "https", hostname: "images.unsplash.com" },
+            { protocol: "https", hostname: "cdn.imweb.me" },
+        ],
+    },
 
-  output: 'standalone',  // dockerize시 standalone 폴더를 사용(빌드 파일 용량 측소)
+    output: "standalone", // dockerize시 standalone 폴더를 사용(빌드 파일 용량 측소)
+};
 
-}
-
-
-module.exports = nextConfig
+module.exports = nextConfig;
